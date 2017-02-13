6d3a28d4912979139085ad53a0909fb681dc336c
David Tsang <eangle@orcon.net.nz>
2017-02-14 03:40:01 +1300

    Update Admin layout page

--- Iscore.WebSite/Iscore.WebSite/App_Start/BundleConfig.cs
+++ Iscore.WebSite/Iscore.WebSite/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Iscore.WebSite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-2.2.3.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,7 +17,8 @@ namespace Iscore.WebSite
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/app.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -25,7 +26,9 @@ namespace Iscore.WebSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/AdminLTE.min.css",
+                      "~/Content/Skins/skin-blue.min.css"));
         }
     }
 }
