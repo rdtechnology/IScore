16607934b5173fc0540e30f7b3aa31ecb2f0ad51
David Tsang <eangle@orcon.net.nz>
2017-03-13 13:44:50 +1300

    add user functions

--- Iscore.WebSite/Iscore.WebSite/Models/AdminModels.cs
+++ Iscore.WebSite/Iscore.WebSite/Models/AdminModels.cs
@@ -43,5 +43,42 @@ namespace Iscore.WebSite.Models
         public string description { get; set; }
     }
 
+    public class UserModel {
+        public int? Id { get; set; }
+        public string name { get; set; }
+        public string firstname { get; set; }
+        public string middlename { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string website { get; set; }
+        public string landline { get; set; }
+        public string mobile { get; set; }
+        public string fax { get; set; }
+        public string role { get; set; }
+        public bool isActive { get; set; }
+        public UserLoginModel userlogin { get; set; }
+        public List<AddressModel> address { get; set; }
+    }
+
+    public class UserLoginModel {
+        public int? Id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool canlogin { get; set; }
+        public int? retryattempt { get; set; }
+    }
+
+    public class AddressModel {
+        public int? Id { get; set; }
+        public int? userId { get; set; }
+        public string streetnumber { get; set; }
+        public string streetaddress { get; set; }
+        public string suburb { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string postcode { get; set; }
+        public string type { get; set; }
+
+    }
 
 }
